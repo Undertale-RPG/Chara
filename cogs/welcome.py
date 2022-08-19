f659da5807afbedea26d92cd03aa1efd87714b30
yakirarage <yaranaafs@gmail.com>
2022-08-19 23:30:28 +0200

    rewrite

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -2,37 +2,32 @@ import disnake
 from disnake.ext import commands, tasks
 
 class Welcome(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
-    self.boost_stats.start()
+	def __init__(self, bot):
+		self.bot = bot
+		self.boost_stats.start()
 
-  @tasks.loop(minutes=5)
-  async def boost_stats(self):
-    await self.bot.wait_until_ready()
-    guild = self.bot.get_guild(817437132397871135)
-    lista = []
-    for i in guild.premium_subscribers:
-      lista.append(i.id)
-
-    await self.bot.boosters.update_one({"_id" : 0}, {"$set" : {"boosters" : lista}})
-    print("Boosters has been updated")
-
-  @commands.slash_command()
-  async def test(self, ctx: disnake.Interaction, member: disnake.Member= commands.Param(description="Choose a member")): 
-    await ctx.response.send_message(content=f"Member passed: {member}")
+	@tasks.loop(minutes=5)
+	async def boost_stats(self):
+		await self.bot.wait_until_ready()
+		guild = self.bot.get_guild(817437132397871135)
+		lista = []
+		for i in guild.premium_subscribers:
+			lista.append(i.id)
+		await self.bot.boosters.update_one({"_id" : 0}, {"$set" : {"boosters" : lista}})
+		print("Boosters has been updated")
     
-  @commands.Cog.listener()
-  async def on_member_join(self, member):
-    embed = disnake.Embed()
-    embed.set_thumbnail(member.guild.icon.url)
-    embed.title=f"Welcome to {member.guild.name}"
-    embed.set_author(name=str(member), icon_url=member.avatar.url)
-    embed.set_footer(text=f"Thanks for joining, your the {member.guild.member_count}th member!", icon_url=self.bot.user.avatar.url)
-    embed.description="• Be sure to read the <#827771691727716353>\n• You can chat with other people at <#827651362099036180>\n • Ask for support and help at <#827651414540943370>!\n• You can get some notification roles at <#846972481511227432>"
-    embed.set_image("https://cdn.discordapp.com/attachments/899309915020095488/900106251256623154/You_Doodle_2021-10-19T19_40_42Z.jpg")
-    cha = self.bot.get_channel(850282417017454602)
-    await cha.send(content=f"* Welcome {member.mention}", embed=embed)
+	@commands.Cog.listener()
+	async def on_member_join(self, member):
+		embed = disnake.Embed()
+		embed.set_thumbnail(member.guild.icon.url)
+		embed.title=f"Welcome to {member.guild.name}"
+		embed.set_author(name=str(member), icon_url=member.avatar.url)
+		embed.set_footer(text=f"Thanks for joining, your the {member.guild.member_count}th member!", icon_url=self.bot.user.avatar.url)
+		embed.description="• Be sure to read the <#827771691727716353>\n• You can chat with other people at <#827651362099036180>\n • Ask for support and help at <#827651414540943370>!\n• You can get some notification roles at <#846972481511227432>"
+		embed.set_image("https://cdn.discordapp.com/attachments/899309915020095488/900106251256623154/You_Doodle_2021-10-19T19_40_42Z.jpg")
+		cha = self.bot.get_channel(850282417017454602)
+		await cha.send(content=f"* Welcome {member.mention}", embed=embed)
     
 
 def setup(bot):
-  bot.add_cog(Welcome(bot))
+	bot.add_cog(Welcome(bot))
--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import disnake
 import os
-import alive
-
+import disnake
+from dotenv import load_dotenv
 from disnake.ext import commands
 from motor.motor_asyncio import AsyncIOMotorClient
 
-class Spamton(commands.Bot):
-  def __init__(self, **kwargs):
-    super().__init__(**kwargs)
-    self.MongoUrl = os.environ["MongoUrl"]
-    self.cluster = AsyncIOMotorClient(self.MongoUrl)
-    self.db = self.cluster["database"]
-    self.boosters = self.db["boosters"]
-    
-bot = Spamton(
-  command_prefix="s!", 
-  intents=disnake.Intents.all(),
-  test_guilds=[817437132397871135],
-  sync_commands_debug=True
-)
-  
-cogs = ["cogs.welcome", "jishaku"]
+load_dotenv()
 
-for i in cogs:
-  bot.load_extension(i)
-  
-alive.keep_alive()
-bot.run(os.environ['TOKEN'])
+description = """The undertale RPG Beta bot."""
+
+intents = disnake.Intents.default()
+intents.members = True
+intents.message_content = True
+
+class SpamtonBot(commands.AutoShardedBot):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.BotToken = os.getenv("TOKEN")
+        self.vote_url = "https://top.gg/bot/815153881217892372"
+        self.activity = disnake.Game("Together with all of you")
+        self.help_command = None
+        self.MongoUrl = os.getenv("MONGO_URL")
+        self.cluster = AsyncIOMotorClient(self.MongoUrl)
+        self.db = self.cluster["database"]
+        self.boosters = self.db["boosters"]
+
+    def load_all_cogs(self):
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py") and not filename.startswith("_"):
+                self.load_extension(f"cogs.{filename[:-3]}")
+                print(f"🔁 cogs.{filename[:-3]} is loaded and ready.")
+        return
+
+
+bot = SpamtonBot(
+    description=description,
+    intents=intents,
+    owner_ids=[536538183555481601, 513351917481623572]
+)
+
+
+bot.load_all_cogs()
+bot.run(bot.BotToken)
