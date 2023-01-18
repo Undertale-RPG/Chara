d98d937d4b672363c374b9e9c379cc35d66437e1
yakirarage <yaranaafs@gmail.com>
2023-01-18 02:30:37 +0100

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -10,7 +10,7 @@ class Info(commands.Cog):
     async def github(self, inter):
         em = disnake.Embed(
             title="Github issue tracker",
-            url="https://top.gg/bot/748868577150369852",
+            url="https://github.com/Undertale-RPG/Undertale-RPG/issues",
             color=0xF99244,
             description="If you wish to report a bug/issue or add a feature request for our bot head over to our Github, there you can create a new issue or add addition information to a already existing issue.\n\n**bug reports and suggestions done outside of this issue tracker are invalid**\n\n*Note: The GitHub issue tracker is public. As such, you should avoid posting sensitive information.*"
         )
