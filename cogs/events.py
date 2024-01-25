bf932a69cd95a98e4914ae2aeb49e56360d651cc
Yara Naafs <yaranaafs@gmail.com>
2024-01-25 22:18:24 +0100

    stupid tab errors

--- cogs/events.py
+++ cogs/events.py
@@ -21,8 +21,8 @@ class Events(commands.Cog):
 				return
 			new_badges = []
 			new_badges.append("booster")
-        	for i in badges:
-            	new_badges.append(i)
+			for i in badges:
+				new_badges.append(i)
 			await self.bot.players.update_one({"_id" : user}, {"$set" : {"badges" : new_badges}})
 			
 		print("Boosters have been updated")
